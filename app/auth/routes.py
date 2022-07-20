b10bac74c1718de80ee34689a7b17dfd24e79d7c
Happy_Warbird <happy_warbird@gmx.net>
2022-07-20 16:28:51 +0200

    Added configitem to disable signup

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, current_app
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
@@ -27,7 +27,8 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
-    return render_template('login.html', title=_('Sign In'), form=form)
+    signup = current_app.config['ALLOW_SIGNUP']
+    return render_template('login.html', title=_('Sign In'), form=form, signup=signup)
 
 
 @bp.route('/logout')
@@ -38,6 +39,8 @@ def logout():
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_app.config['ALLOW_SIGNUP'] is False:
+        return redirect(url_for('auth.login'))
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
     form = RegistrationForm()
