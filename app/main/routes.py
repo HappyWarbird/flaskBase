4d5ed1953ab2201855babe0bc4a524c46f39da0c
Happy_Warbird <happy_warbird@gmx.net>
2022-07-21 09:54:43 +0200

    removed language var

--- app/main/routes.py
+++ app/main/routes.py
@@ -23,7 +23,7 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user, language=language)
+        post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
